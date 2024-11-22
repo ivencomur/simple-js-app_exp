4dc46feca364f573203f46ee525c0c0559cc6cef
ivencomur <ivancortes@hotmail.com>
2024-11-21 19:54:25 -0500

    INITIAL COMMIT
    All changes made and steps followed per the task directions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-alert('Hello world');
-console.log('Hello world');
-
-let favoriteFood = 'Mushrooms & French fries';
-document.write(favoriteFood);
-
-
-
-
-
-
+let PokemonList = [{name: "Butterfree", height: "1.1m", types: ["bug", "flying"]},
+    {name: "Venomoth", height: "1.5m", "types: ["bug", "poison"]},
+    {name: "Rhydon", height: "1.9m", types: ["rock", "ground"]},
+    {name: "Articuno", height: "1.7m", types: ["ice", "flying"]}];
