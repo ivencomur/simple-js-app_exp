b206121b22a7bc2a3afbac4e194fa45ef6a95f29
ivencomur <ivancortes@hotmail.com>
2024-11-21 20:38:12 -0500

    RELOADED

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-let PokemonList = [{name: "Butterfree", height: "1.1m", types: ["bug", "flying"]},
-    {name: "Venomoth", height: "1.5m", "types: ["bug", "poison"]},
-    {name: "Rhydon", height: "1.9m", types: ["rock", "ground"]},
-    {name: "Articuno", height: "1.7m", types: ["ice", "flying"]}];
+alert('Hello world');
+console.log('Hello world');
+
+let favoriteFood = 'Mushrooms & French fries';
+document.write(favoriteFood);
+
+
+
+
+
+
